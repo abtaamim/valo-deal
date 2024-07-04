b62671f807a86a05af9ed4858ea5266358f0cacb
abtaamim <abdullahaltamim001@gmail.com>
2024-07-04 22:12:07 +0600

    vercel_cors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,13 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://valo-deal-frontend.vercel.app"],
+    methods: ['POST', 'GET'],
+    credentials: true
+  }
+));
 app.use(express.json());
 app.use(morgan("dev"));
 
