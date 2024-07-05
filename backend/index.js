84e13e77bb3426b5f75f734d3d83146a057f1667
abtaamim <142093936+abtaamim@users.noreply.github.com>
2024-07-06 03:01:23 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors(
   {
     origin: ["https://valo-deal-frontend.vercel.app"],
-    methods: ['POST', 'GET'],
+    methods: ['POST', 'GET', 'PUT', 'DELETE'],
     credentials: true
   }
 ));
