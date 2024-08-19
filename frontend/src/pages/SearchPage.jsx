4a33d311e02a7c3e91cc986f696d620a5f119228
abtaamim <142093936+abtaamim@users.noreply.github.com>
2024-08-19 23:40:16 +0600

    Update SearchPage.jsx adding vercel link

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -63,7 +63,7 @@ const SearchPage = () => {
   const navigate = useNavigate();
   const handleRecentlyView = async (itemId, itemType) => {
     try {
-      await axios.post(`http://localhost:8080/recentlyViewed/${itemType}/${itemId}`);
+      await axios.post(`https://valo-deal-backend.vercel.app/recentlyViewed/${itemType}/${itemId}`);
     } catch (error) {
       console.error(`Error viewing item:`, error);
     }
@@ -75,7 +75,7 @@ const SearchPage = () => {
     const fetchResults = async () => {
       if (keyword) {
         try {
-          const res = await axios.get(`http://localhost:8080/search/${keyword}`);
+          const res = await axios.get(`https://valo-deal-backend.vercel.app/search/${keyword}`);
           setValues({ ...values, results: res.data.results });
         } catch (error) {
           console.log('Error:', error);
@@ -99,7 +99,7 @@ const SearchPage = () => {
   const handleCart = async (itemId, itemType) => {
     try {
       if (auth.user) {
-        await axios.post(`http://localhost:8080/cart/${itemType}/${itemId}`);
+        await axios.post(`https://valo-deal-backend.vercel.app/cart/${itemType}/${itemId}`);
         // fetchItems(); // Refresh items after deletion
         updateCartSize();
         toast.success('item added to your cart successfully')
@@ -122,7 +122,7 @@ const SearchPage = () => {
       const sellerIds = new Set(values.results.map((item) => item.sellerId));
 
       const sellerPromises = Array.from(sellerIds).map((sellerId) =>
-        axios.get(`http://localhost:8080/api/v1/auth/seller-info/${sellerId}`)
+        axios.get(`https://valo-deal-backend.vercel.app/api/v1/auth/seller-info/${sellerId}`)
       );
 
       const sellerResponses = await Promise.all(sellerPromises);
